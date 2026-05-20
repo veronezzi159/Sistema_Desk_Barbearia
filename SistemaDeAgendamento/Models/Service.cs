7a0e19bbef6af093f81b4ec8cb5a74fa5846bdd3
lopespp <rlopespietro@gmail.com>
2026-05-20 16:01:18 -0300

    Update: line removed

--- SistemaDeAgendamento/Models/Service.cs
+++ SistemaDeAgendamento/Models/Service.cs
@@ -9,7 +9,6 @@ namespace SistemaDeAgendamento.Models
     public class Service
     {
         public int Id { get; set; }
-
         public string Description { get; set; }
         public string Name { get; set; } 
         public string ServicePrice { get; set; } 
