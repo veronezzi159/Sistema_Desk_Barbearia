f209c03918b3c3fe3883cf1f31949578ef8de2a9
veronezzi159 <lucasveronezzi3@gmail.com>
2026-05-21 15:27:27 -0300

    add: form service

---
+++ SistemaDeAgendamento/Forms/FormService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeAgendamento.Forms
+{
+    public partial class FormService : Form
+    {
+        public FormService()
+        {
+            InitializeComponent();
+        }
+    }
+}
